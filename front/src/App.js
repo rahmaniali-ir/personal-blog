ee020dcbcf75cf5314723febe1c6cb0298250bfe
Ali Rahmani <alirahmani.prog@gmail.com>
2021-03-15 15:45:24 +0330

    Initial commit

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='App'>
+      <nav id='main-nav'>Navbar here!</nav>
+      <main id='main-wrapper'>Main</main>
+    </div>
+  )
+}
+
+export default App
